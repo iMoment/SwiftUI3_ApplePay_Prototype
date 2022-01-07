652fbbf460ed4fbc64127f407df0533b5d6c4d6a
Stanley Pan <imoment.developer@gmail.com>
2022-01-07 15:01:03 -0500

    Initial Commit

---
+++ Shopping_ApplePay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shopping_ApplePay
+//
+//  Created by Stanley Pan on 2022/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shopping_ApplePay/Shopping_ApplePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ApplePayApp.swift
+//  Shopping_ApplePay
+//
+//  Created by Stanley Pan on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ApplePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
