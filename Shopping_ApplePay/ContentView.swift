84c335353eaf9df3f111a7e7c25d0211616d5813
Stanley Pan <imoment.developer@gmail.com>
2022-01-07 16:07:47 -0500

    Added CartButton component, implemented ProductCard and CartButton into ContentView

--- Shopping_ApplePay/ContentView.swift
+++ Shopping_ApplePay/ContentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    var columns = [
+        GridItem(.adaptive(minimum: 160), spacing: 20)
+    ]
+    
     var body: some View {
-        Text("Apple Pay Prototype")
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: true) {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Sweater Shop"))
+            .toolbar {
+                CartButton(numberOfProducts: 1)
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
