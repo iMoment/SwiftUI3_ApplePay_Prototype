dd5964cbef385815f838238e2461b5c653f93412
Stanley Pan <imoment.developer@gmail.com>
2022-01-12 15:13:58 -0500

    Added PaymentButton component, implemented in CartView

--- Shopping_ApplePay/Views/CartView.swift
+++ Shopping_ApplePay/Views/CartView.swift
@@ -26,6 +26,9 @@ struct CartView: View {
                         .bold()
                 }
                 .padding()
+                
+                PaymentButton(action: {})
+                    .padding()
             } else {
                 Text("Your cart is empty")
             }
