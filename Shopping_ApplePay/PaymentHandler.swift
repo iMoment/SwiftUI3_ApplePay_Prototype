50a521c9dc8eecb839d84bc4302ddab23ff1e503
Stanley Pan <imoment.developer@gmail.com>
2022-01-14 15:13:23 -0500

    Added PaymentHandler service, Entitlement not set up yet on developer account

---
+++ Shopping_ApplePay/PaymentHandler.swift
@@ -0,0 +1,48 @@
+//
+//  PaymentHandler.swift
+//  Shopping_ApplePay
+//
+//  Created by Stanley Pan on 2022/01/14.
+//
+
+import Foundation
+import PassKit
+
+typealias PaymentCompletionHandler = (Bool) -> Void
+
+class PaymentHandler: NSObject {
+    var paymentController: PKPaymentAuthorizationController?
+    var paymentSummaryItems = [PKPaymentSummaryItem]()
+    var paymentStatus = PKPaymentAuthorizationStatus.failure
+    var completionHandler: PaymentCompletionHandler?
+    
+    // MARK: Supported Cards
+    static let supportedNetworks: [PKPaymentNetwork] = [
+        .amex,
+        .discover,
+        .masterCard,
+        .visa
+    ]
+    
+    func shippingMethodCalculator() -> [PKShippingMethod] {
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let shippingStart = calendar.date(byAdding: .day, value: 5, to: today)
+        let shippingEnd = calendar.date(byAdding: .day, value: 10, to: today)
+        
+        if let shippingStart = shippingStart, let shippingEnd = shippingEnd {
+            let startComponents = calendar.dateComponents([.calendar, .year, .month, .day], from: shippingStart)
+            let endComponents = calendar.dateComponents([.calendar, .year, .month, .day], from: shippingEnd)
+            
+            let shippingDelivery = PKShippingMethod(label: "Delivery", amount: NSDecimalNumber(string: "0.00"))
+            shippingDelivery.dateComponentsRange = PKDateComponentsRange(start: startComponents, end: endComponents)
+            shippingDelivery.detail = "Sweaters were sent to your address"
+            shippingDelivery.identifier = "DELIVERY"
+            
+            return [shippingDelivery]
+        }
+        
+        return []
+    }
+}
